d83f1c4c1d486a9826cbbcd4773cef4b365a2d1f
Oak Soe Kha <ppwhtin@gmail.com>
2024-05-02 16:06:31 +0630

    Added functions to visualize data

---
+++ app/utils.py
@@ -0,0 +1,32 @@
+import plotly.express as px
+import numpy as np
+import pandas as pd
+
+
+months = np.arange(1, 13)
+
+
+def ElectricityBill(kWh) -> None:
+    fig = px.line(x=months, y=kWh, labels={
+                  "x": "Months", "y": "Kilo-watt hours"}, markers=True)
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[
+                      str(month) for month in months]))
+    fig.show()
+
+
+def WaterBill(cubic_m) -> None:
+    fig = px.line(x=months, y=cubic_m, labels={
+                  "x": "Months", "y": "Cubic Meters"}, markers=True)
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[
+                      str(month) for month in months]))
+    fig.show()
+
+
+def RevenueToCF(revenue: np.array, CF: np.array, names: np.array) -> None:
+    fig = px.scatter(x=revenue, y=CF, labels={"x": "Revenue", "y": "Carbon Emissions"},
+                     hover_name=names, size=revenue, color=CF)
+    fig.update_traces(text=names)
+    fig.update_traces(marker=dict(opacity=1))
+    fig.update_traces(
+        hovertemplate="<b>%{hovertext}</b><br>Revenue: %{x}<br>Carbon Emissions: %{y}")
+    fig.show()
