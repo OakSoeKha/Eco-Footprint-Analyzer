044ce7e536938b8738fcb4b7e006cf8d47d71230
Oak Soe Kha <ppwhtin@gmail.com>
2024-05-16 09:35:54 +0630

    Changed graphs color

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-from routes import *
 from flask import Flask
 from models import calculate_carbon_footprint
 from utils import *
 
 app = Flask(__name__)
+
+from routes import *
+
 
 
 if __name__ == "__main__":
--- app/utils.py
+++ app/utils.py
@@ -1,53 +1,74 @@
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
+import random
+
+months = ["January", "Febuary", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"]
 
 
-months = np.arange(1, 13)
+def ElectricityBill(kWh, multiple_series=False) -> None:
+    fig = go.Figure()
+    if not multiple_series:
+        fig.add_trace(go.Scatter(x=months, y=kWh,
+                      mode='lines+markers', name='Data'))
+    else:
+        for i, data in enumerate(kWh):
+            fig.add_trace(go.Scatter(
+                x=months, y=data, mode='lines+markers', name=f'Data {i+1}'))
 
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[str(month) for month in months]),
+                      yaxis=dict(title='Kilo-watt hours'),
+                      template='plotly_dark')
 
-def ElectricityBill(kWh) -> None:
-    fig = px.line(x=months, y=kWh, labels={
-                  "x": "Months", "y": "Kilo-watt hours"}, markers=True)
-    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[
-                      str(month) for month in months]))
     fig.write_html(
-        "eco-footprint-analyzer/app/static/graphs/electricity-bill.html")
+        "app/static/graphs/electricity-bill.html")
 
 
-def WaterBill(cubic_m) -> None:
-    fig = px.line(x=months, y=cubic_m, labels={
-                  "x": "Months", "y": "Cubic Meters"}, markers=True)
-    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[
-                      str(month) for month in months]))
-    fig.write_html(
-        "eco-footprint-analyzer/app/static/graphs/water-bill.html")
+def WaterBill(cubic_m, multiple_series=False) -> None:
+    fig = go.Figure()
+    if not multiple_series:
+        fig.add_trace(go.Scatter(x=months, y=cubic_m,
+                      mode='lines+markers', name='Data'))
+    else:
+        for i, data in enumerate(cubic_m):
+            fig.add_trace(go.Scatter(
+                x=months, y=data, mode='lines+markers', name=f'Data {i+1}'))
+
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=months, ticktext=[str(month) for month in months]),
+                      yaxis=dict(title='Cubic Meters'),
+                      template='plotly_dark')
+
+    fig.write_html("app/static/graphs/water-bill.html")
 
 
 def RevenueToCF(revenue: np.array, CF: np.array, names: np.array) -> None:
-    fig = px.scatter(x=CF, y=revenue, labels={"x": "Carbon Emissions", "y": "Revenue"},
-                     hover_name=names, size=revenue)
+    fig = px.scatter(x=CF, y=revenue, labels={
+                     "x": "Carbon Emissions", "y": "Revenue"}, hover_name=names, size=revenue)
     fig.update_traces(text=names)
     fig.update_traces(marker=dict(opacity=1))
     fig.update_traces(
         hovertemplate="<b>%{hovertext}</b><br>Carbon Emissions: %{x} billion tonnes<br>Revenue: %{y} billion dollars")
-    fig.write_html(
-        "eco-footprint-analyzer/app/static/graphs/revenue-cf.html")
+    fig.update_layout(template='plotly_dark')
+    fig.write_html("app/static/graphs/revenue-cf.html")
 
 
 def EmissionsBar(**kwargs) -> None:
-    fig = px.bar(x=kwargs["months"], y=kwargs["emission"],
+    fig = px.bar(x=months, y=kwargs["emission"],
                  labels={"x": "Months", "y": "Emissions"})
     fig.update_traces(hoverinfo='text', hovertext=[f'{name}: {
-                      value}' for name, value in zip(kwargs["months"], kwargs["emission"])])
-    fig.write_html("eco-footprint-analyzer/app/static/graphs/emissions.html")
+                      value}' for name, value in zip(months, kwargs["emission"])])
+    fig.update_layout(template='plotly_dark')
+    fig.write_html("app/static/graphs/emissions.html")
 
 
 def PercentageChart(**kwargs) -> None:
-    fig = px.pie(names=kwargs.keys(), values=kwargs.values())
+    labels = list(kwargs.keys())
+    values = list(kwargs.values())
+
+    fig = go.Figure(go.Pie(labels=labels, values=values))
     fig.update_traces(hoverinfo='label+percent')
+    fig.update_layout(template='plotly_dark')
+
     fig.write_html(
-        "eco-footprint-analyzer/app/static/graphs/percentage-chart.html")
-
-
-EmissionsBar(months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-             "Sem", "Oct", "Nov", "Dec"], emission=[1, 5, 2, 7, 1, 9, 5, 2, 9, 3, 4, 2])
+        "app/static/graphs/percentage-chart.html")
